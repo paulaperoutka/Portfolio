ac985f71a1de2640e4068ce9a0f45fa171c1f461
paulaperoutka <paulaperoutka@gmail.com>
2018-05-12 22:55:21 -0400

    main section running

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//   $(".sidenav").open();
+// });
+
+$(document).ready(function(){
+
+	$("#splash-name").fadeIn(10000);
+
+	$("#splash-name").on("click", () => {
+			location.href="portfolio.html";
+	});
+
+});
+
+
+// $("#splash-name").on("click", function () {
+
+// })
